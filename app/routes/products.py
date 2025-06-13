f496299b79a372790dabda757bfcb256460eb3cd
Mincai Wang <mincaiw@yonsei.com>
2025-06-14 01:01:17 +0900

    Products/Products.details

--- app/routes/products.py
+++ app/routes/products.py
@@ -7,3 +7,8 @@ products_bp = Blueprint('products_bp', __name__, url_prefix='/products')
 def show_products():
     products = Product.query.all()
     return render_template('products.html', products=products)
+
+@products_bp.route('/<int:product_id>')
+def product_detail(product_id):
+    product = Product.query.get_or_404(product_id)
+    return render_template('products/products_detail.html', product=product)
